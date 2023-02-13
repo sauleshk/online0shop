e8aa1adb0b9455bb1bdfb152ecd5c1c7bdca9299
sauleshk <saureika06@gmail.com>
2023-02-13 21:38:55 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-4/5 text-center bg-orange-300 h-screen">
+      <h1 className='text-red-500'>Hello Tailwind!</h1>
     </div>
   );
 }
